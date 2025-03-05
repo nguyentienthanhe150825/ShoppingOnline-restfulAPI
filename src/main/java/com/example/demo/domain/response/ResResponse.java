8dbe4add99243c0cd31421d6d6d1149f5e2985b7
nguyentienthanhe150825 <nguyenthanh2542001@gmail.com>
2025-03-05 13:56:55 +0700

    Handle Global Exception and Config Error NoSuchMethodError Swagger in file pom.xml

---
+++ src/main/java/com/example/demo/domain/response/ResResponse.java
@@ -0,0 +1,61 @@
+package com.example.demo.domain.response;
+
+import java.util.Date;
+
+public class ResResponse<T> {
+    // https://stackoverflow.com/questions/24072458/how-can-i-wrap-a-json-response-in-spring/52104852#52104852
+    private int statusCode;
+    private String message;
+    private String error;
+    private Date timestamp;
+    private String path;
+    private T data;
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/com/example/demo/util/exception/GlobalException.java
@@ -0,0 +1,35 @@
+package com.example.demo.util.exception;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import com.example.demo.domain.response.ResResponse;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResResponse<Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex,
+            WebRequest request) {
+        ResResponse<Object> res = new ResResponse<>();
+        res.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        res.setError(ex.getBody().getDetail());
+        res.setTimestamp(new Date());
+        res.setPath(request.getDescription(false).replace("uri=", ""));
+
+        String message = ex.getBindingResult().getFieldErrors()
+                .stream().map(err -> err.getDefaultMessage())
+                .findFirst()
+                .orElse("Validation error");
+
+        res.setMessage(message);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
+    }
+}
