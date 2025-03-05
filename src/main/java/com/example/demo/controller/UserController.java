8f0d85e6108f9b758512b7e9804581dfba1e8a6a
nguyentienthanhe150825 <nguyenthanh2542001@gmail.com>
2025-03-05 10:59:03 +0700

    API Create User

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,5 +22,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    
+    @PostMapping("/users")
+    public ResponseEntity<User> createNewUser(@Valid @RequestBody User createUser) {
+        User newUser = this.userService.handleCreateUser(createUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+    }
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
 
 @Service
@@ -9,7 +10,11 @@ public class UserService {
     // Dependency Inject: Constructor
     private final UserRepository userRepository;
 
-    public UserService (UserRepository userRepository) {
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User handleCreateUser(User createUser) {
+        return this.userRepository.save(createUser);
     }
 }
