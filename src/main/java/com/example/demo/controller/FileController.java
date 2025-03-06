8a281113da9942d3f6b6ba0ad3dfb99d1d58e284
Tien Thanh <nguyenthanh2542001@gmail.com>
2025-03-06 22:14:53 +0700

    Download File

---
+++ src/main/java/com/example/demo/controller/FileController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controller;
+
+import java.io.FileNotFoundException;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.FileService;
+import com.example.demo.util.exception.StorageException;
+
+@RestController
+@RequestMapping("/api/v1")
+public class FileController {
+
+    @Value("${tomosia.upload-file.base-uri}")
+    private String baseURI;
+
+    private final FileService fileService;
+
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @GetMapping("/files")
+    public ResponseEntity<Resource> downloadFile(
+            @RequestParam(name = "fileName", required = false) String fileName,
+            @RequestParam(name = "folder", required = false) String folder) throws StorageException, URISyntaxException, FileNotFoundException {
+
+        // check file or folder exist
+        if (fileName == null || folder == null) {
+            throw new StorageException("Missing request param: (fileName or folder not exist)");
+        }
+
+        // check file exist (and not a directory)
+        long fileLength = this.fileService.getFileLength(fileName, baseURI + folder);
+        if (fileLength == 0) {
+            throw new StorageException("File with name = " + fileName + " not found");
+        }
+
+        // download single file
+        InputStreamResource resource = this.fileService.getResource(fileName, baseURI + folder);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .contentLength(fileLength)
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
+}
---
+++ src/main/java/com/example/demo/service/FileService.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+
+    public long getFileLength(String fileName, String folder) throws URISyntaxException {
+        URI uri = new URI(folder + "/" + fileName);
+        Path path = Paths.get(uri);
+
+        File tmpDir = new File(path.toString());
+
+        // file not exist or file is 1 directory (thư mục thì sẽ ko có kích thước tệp)
+        if (!tmpDir.exists() || tmpDir.isDirectory()) {
+            return 0;
+        }
+        return tmpDir.length();
+    }
+
+    public InputStreamResource getResource(String fileName, String folder) throws URISyntaxException, FileNotFoundException {
+        URI uri = new URI(folder + "/" + fileName);
+        Path path = Paths.get(uri);
+
+        File file = new File(path.toString());
+        return new InputStreamResource(new FileInputStream(file));
+    }
+    
+}
