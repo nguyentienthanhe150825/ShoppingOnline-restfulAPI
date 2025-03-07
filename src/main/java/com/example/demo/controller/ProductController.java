d90f7613a41242887aaafc1e74d78c151d916a66
nguyentienthanhe150825 <nguyenthanh2542001@gmail.com>
2025-03-07 17:41:41 +0700

    API Delete Product By Id

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -146,6 +147,16 @@ public class ProductController {
         ResultPaginationDTO result = this.productService.fetchAllProductsWithPagination(spec, pageable);
 
         return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable("id") long id) throws InvalidException {
+        Product currentProduct = this.productService.handleGetProducById(id);
+        if (currentProduct == null) {
+            throw new InvalidException("Product with id = " + id + " not exist");
+        }
+        this.productService.handleDeleteProduct(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Delete Success");
     }
 
 }
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
@@ -7,18 +8,22 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.domain.OrderDetail;
 import com.example.demo.domain.Product;
 import com.example.demo.domain.response.Meta;
 import com.example.demo.domain.response.ResultPaginationDTO;
+import com.example.demo.repository.OrderDetailRepository;
 import com.example.demo.repository.ProductRepository;
 
 @Service
 public class ProductService {
     // Dependency Inject: Constructor
     private final ProductRepository productRepository;
+    private final OrderDetailRepository orderDetailRepository;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, OrderDetailRepository orderDetailRepository) {
         this.productRepository = productRepository;
+        this.orderDetailRepository = orderDetailRepository;
     }
 
     public Product handleGetProducById(long id) {
@@ -66,6 +71,18 @@ public class ProductService {
         currentProduct.setBrand(requestProduct.getBrand());
 
         return this.productRepository.save(currentProduct);
+    }
+
+    public void handleDeleteProduct(long id) {
+        Product currentProduct = this.handleGetProducById(id);
+        List<OrderDetail> orderDetails = currentProduct.getOrderDetails();
+        for (OrderDetail orderDetail : orderDetails) {
+            // Delete Order-detail
+            this.orderDetailRepository.deleteById(orderDetail.getId());
+        }
+
+        // Delete product
+        this.productRepository.deleteById(id);
     }
 
 }
